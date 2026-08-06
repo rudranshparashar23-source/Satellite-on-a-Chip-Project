734341d8a8d7699ec0bb5eda0600e8a5baeb69ea
rudranshparashar23-source <rudranshparashar23@gmail.com>
2026-08-06 23:00:03 +0530

    Refactor eps_top entity to eps with updated signals

--- rtl/eps/eps_top.vhd
+++ rtl/eps/eps_top.vhd
@@ -2,51 +2,40 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity eps_top is
+entity eps is
     Port (
-        clk             : in  STD_LOGIC;
-        reset           : in  STD_LOGIC;
-
-        battery_voltage : in  STD_LOGIC_VECTOR(7 downto 0);
-        battery_current : in  STD_LOGIC_VECTOR(7 downto 0);
-
-        power_ok        : out STD_LOGIC;
-        hk_data         : out STD_LOGIC_VECTOR(15 downto 0)
+        clk              : in  STD_LOGIC;
+        reset            : in  STD_LOGIC;
+        battery_voltage  : in  STD_LOGIC_VECTOR(7 downto 0);
+        battery_current  : in  STD_LOGIC_VECTOR(7 downto 0);
+        power_status     : out STD_LOGIC;
+        hk_data          : out STD_LOGIC_VECTOR(15 downto 0)
     );
-end eps_top;
+end eps;
 
-architecture Behavioral of eps_top is
-
-signal voltage_reg : STD_LOGIC_VECTOR(7 downto 0);
-signal current_reg : STD_LOGIC_VECTOR(7 downto 0);
+architecture Behavioral of eps is
 
 begin
 
-process(clk, reset)
+    process(clk, reset)
+    begin
 
-begin
+        if reset = '1' then
+            power_status <= '0';
+            hk_data <= (others => '0');
 
-    if reset='1' then
+        elsif rising_edge(clk) then
 
-        voltage_reg <= (others=>'0');
-        current_reg <= (others=>'0');
-        power_ok    <= '0';
+            hk_data <= battery_voltage & battery_current;
 
-    elsif rising_edge(clk) then
+            if unsigned(battery_voltage) > 40 then
+                power_status <= '1';
+            else
+                power_status <= '0';
+            end if;
 
-        voltage_reg <= battery_voltage;
-        current_reg <= battery_current;
-
-        if unsigned(battery_voltage) > 40 then
-            power_ok <= '1';
-        else
-            power_ok <= '0';
         end if;
 
-    end if;
-
-end process;
-
-hk_data <= voltage_reg & current_reg;
+    end process;
 
 end Behavioral;
